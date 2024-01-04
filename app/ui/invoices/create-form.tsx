cc8cd5e5c8af277a8223429b7af31e1e54c9d742
owocc <owocc@qq.com>
2024-01-04 21:28:19 +0800

    添加修改和删除

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -7,7 +7,7 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
-import { createInvoices } from '@/app/lib/actinos';
+import { createInvoices } from '@/app/lib/actions';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
